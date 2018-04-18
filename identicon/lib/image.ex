cfc156ac935af883259cb47257b409f61315228a
Shaunna Wiens <shaunna.wiens@socrata.com>
2018-04-18 08:39:05 -0700

    initial elixir files, main function to call hash_input and hash_input to turn string into a list of 16 numbers with range to 255, image struct to store details

---
+++ identicon/lib/image.ex
@@ -0,0 +1,12 @@
+defmodule Identicon.Image do
+  defstruct hex: nil
+
+end
+
+# A struct is like a map with two additional features
+   # It can be assigned default values
+   # It has some additional compile time checking and properties
+
+# Struct over a map:
+  # A struct enforces that it will only have the properties defined in the module.
+  # A normal map will let you defined any properties you want
